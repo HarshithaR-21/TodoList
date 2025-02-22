9aa64b8260ffe35a71cfde4aa050d4620fcce890
HarshithaR-21 <harshithahr67@gmail.com>
2025-02-22 15:19:43 +0530

    added pic

--- client/todolist/src/Home.jsx
+++ client/todolist/src/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
 
     return (
 
-        <div className="sm:bg-bannerImg bg-mobileImg bg-no-repeat bg-cover">
+        <div className="sm:bg-bannerImg bg-mobileImg bg-no-repeat bg-center bg-cover min-h-screen w-full">
 
             <nav className="p-6 m-0">
                 <Sidebar/>

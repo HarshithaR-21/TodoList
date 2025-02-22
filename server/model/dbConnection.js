9cc651404e9585fb0d98c47610dec5608a9c8ac3
HarshithaR-21 <harshithahr67@gmail.com>
2025-02-22 12:43:52 +0530

    added mongourl

--- server/model/dbConnection.js
+++ server/model/dbConnection.js
@@ -1,6 +1,9 @@
 const {MongoClient} = require('mongodb');
 require('dotenv').config();
-const client = new MongoClient(process.env.MONGO_URL);
+const client = new MongoClient(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 const dbConnection = async () => {
     await client.connect()
